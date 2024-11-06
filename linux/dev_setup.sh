0bfb8905e7142533aefb575a6cb03ab00d513328
Ksawier Malkiewicz <ksawier.helix@gmail.com>
2024-11-06 17:31:02 +0100

    Update dev_setup.sh

--- linux/dev_setup.sh
+++ linux/dev_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Clear the screen and display the header
 clear
@@ -23,6 +23,9 @@ sudo apt install curl wget zsh git jq -y
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Change to Zsh for the remainder of the script
+exec zsh
+
 # Install NVM
 latest_version=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
 
@@ -30,45 +33,31 @@ latest_version=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest
 echo "Latest NVM version: $latest_version"
 
 # Download the installation script for the latest version of NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$latest_version/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$latest_version/install.sh | zsh
 
-# Add NVM configuration to .bashrc and .zshrc
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
-echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
-
-# Add pnpm alias to .bashrc and .zshrc
-echo 'alias pn=pnpm' >> ~/.bashrc
-echo 'alias pn=pnpm' >> ~/.zshrc
-
-# Reload shell configuration
-source ~/.bashrc
-
-# Install the latest LTS version of Node.js
-nvm install --lts
+# Load NVM and install Node.js
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"  # Load NVM
+nvm install --lts          # Install the latest LTS version of Node.js
 nvm use --lts
 
 # Install pnpm
-npm i -g pnpm
+npm install -g pnpm
 
 # Configure pnpm for global packages
 pnpm setup
 
-# Add pnpm configuration to .bashrc and .zshrc
-echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.bashrc
-echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.bashrc
+# Add pnpm configuration to .zshrc
 echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.zshrc
 echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.zshrc
 
-# Reload shell configuration again
-source ~/.bashrc
+# Reload shell configuration
 source ~/.zshrc
 
 # Install vercel-cli
-pnpm i -g vercel@latest
+pnpm install -g vercel@latest
 
-# Connect to vercel account
+# Connect to Vercel account
 vercel login
 vercel whoami
 
@@ -78,8 +67,7 @@ chsh -s $(which zsh)
 # Create tmp directory and download JetBrains Toolbox
 mkdir -p ~/tmp
 cd ~/tmp
-
-curl -o- https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/refs/heads/master/jetbrains-toolbox.sh | bash
+curl -o- https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/refs/heads/master/jetbrains-toolbox.sh | zsh
 
 # Remove tmp directory
 rm -rf ~/tmp
